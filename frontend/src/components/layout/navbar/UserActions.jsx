1d8558d398824a814befbaf42af4b06ac4c36276
Andrei25i <andreiiosif2504@gmail.com>
2025-12-03 19:22:56 +0200

    feat: Add sign out button

--- frontend/src/components/layout/navbar/UserActions.jsx
+++ frontend/src/components/layout/navbar/UserActions.jsx
@@ -1,14 +1,29 @@
 import { Link } from "react-router-dom";
+import useAuthStore from "../../../store/authStore";
 import Heart from "../../icons/Heart";
 import styles from "./Navbar.module.css";
 
 const UserActions = ({ user }) => {
+  const logout = useAuthStore((state) => state.logout);
+  const handleLogout = () => {
+    logout(); 
+    window.location.href = "/"; 
+  };
+
   return (
     <div className={styles.userActions}>
-      <Link to="/profile" className={styles.userAvatar}>
-        <div>{user.first_name[0] + user.last_name[0]}</div>
-        <p>Cont</p>
-      </Link>
+      <div className={styles.profileWrapper}>
+        <Link to="/profile" className={styles.userAvatar}>
+          <div>{user.first_name[0] + user.last_name[0]}</div>
+          <p>Cont</p>
+        </Link>
+
+        <div className={styles.dropdownMenu}>
+          <button onClick={handleLogout} className={styles.signOutButton}>
+            Sign Out
+          </button>
+        </div>
+      </div>
 
       <Link to="/favorites" className={styles.favorites}>
         <Heart className={styles.heartIcon} />
